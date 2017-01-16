09ad77a78804e5a6480f3249e18d5221fe7a4e99
sr247 <stephane@Optimus-Prime.com>
2017-01-16 11:44:05 +0100

    Ajout - JComboBox, JList, ActionListner sur Open/Cancel, JComboBox, Jlist, et correction de additions des dossier et du parcours des répertoires

--- TD1/e0/FileSelector.java
+++ TD1/e0/FileSelector.java
@@ -2,10 +2,18 @@
 //package e3;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Vector;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
@@ -25,37 +33,90 @@ public class FileSelector extends JDialog {
 	public FileSelector(String title, String lbl_cancel, String lbl_ok) {
 		super((JFrame) null, title, true /* modal */);
 
-		Container pan = this.getContentPane();
-		pan.setLayout(new BorderLayout());
+		String[] dirTab = directory.split("/");
+		ArrayList<String> fileTab = new ArrayList<String>();
+		file = "";
+		Vector model = new Vector();
+		
 		JComboBox mainBox = new JComboBox();
+		mainBox.addItemListener(new ItemListener(){
+			public void itemStateChanged(ItemEvent e) {
+			      System.out.println("événement déclenché sur : " + e.getItem());
+					File f = new File(file);
+					for (File fs : f.listFiles()) {
+						System.out.println("#" + fs.getName());
+						model.add(fs.isDirectory() ? fs.getName() + "/" : fs.getName());
+					}
+			    }
+			
+		});
 
 		show(System.getProperty("user.dir"));
 
-		String[] dirTab = directory.split("/");
+		
+		
+
 		for (String s : dirTab) {
-			System.out.println(s);
+			file += s + "/";
+			fileTab.add(file);
+			System.out.println("Dossier en cours :" + file);
 			mainBox.addItem(s);
+
 		}
 
-		JList fileList = new JList();
-		for (String s : dirTab) {
-			System.out.println(s);
-			mainBox.addItem(s);
-		}
+		// File f = new File("/home/stephane");
+		// File[] fs = f.listFiles();
+		// System.out.println(fs[0].getName());
+
+		JList fileList = new JList(model);
+		fileList.addMouseListener(new MouseAdapter() {
+
+			public void mouseClicked(MouseEvent e) {
+
+			}
+
+		});
 
 		JButton open = new JButton(lbl_ok);
+		open.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("ActionListener : action sur " + fileList.getSelectedValue());				
+				Color color1 = new Color(255, 0, 0, 128);
+				fileList.setSelectionBackground(color1);
+				/*
+				 * Ici on fera open : si fichier alors on l'ouvre
+				 * 
+				 * TODO
+				 * 
+				 * Si Directory alors on l'ouvre dans la JcomboBox
+				 */
+			}
+		});
+
 		JButton cancel = new JButton(lbl_cancel);
+		cancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
 
 		JPanel pBottom = new JPanel();
 		pBottom.add(cancel, BorderLayout.SOUTH);
 		pBottom.add(open, BorderLayout.SOUTH);
 
+		Container pan = this.getContentPane();
+		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		pan.setLayout(new BorderLayout());
 		pan.add(mainBox, BorderLayout.NORTH);
 		pan.add(fileList, BorderLayout.CENTER);
 		pan.add(pBottom, BorderLayout.SOUTH);
 
 		pack();
 		setVisible(true);
+	}
+
+	void JBoxDirSelector() {
+
 	}
 
 	Boolean show(String path) {
